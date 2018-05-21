a4b5378ec64292d1629e7968243c6dd591f1e8bb
Boyuan Chen <mentalchen@gmail.com>
2018-05-21 10:57:48 -0700

    finish simpleteleop

---
+++ ControlStation/SimpleTeleOp.py
@@ -0,0 +1,56 @@
+import socket
+import cv2
+import numpy as np
+import sys
+
+def nothing(x):
+    pass
+
+TCP_IP = '10.0.0.76'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+# Create a black image, a window
+img = np.zeros((300,512,3), np.uint8)
+cv2.namedWindow('image')
+
+# create trackbars for color change
+cv2.createTrackbar('L','image',  64, 127 ,nothing)
+cv2.createTrackbar('R','image', 64 , 127 ,nothing)
+
+# create switch for ON/OFF functionality
+switch = '0 : OFF \n1 : ON'
+cv2.createTrackbar(switch, 'image', 0, 1,nothing)
+
+while(1):
+    cv2.imshow('image',img)
+    k = cv2.waitKey(10) & 0xFF
+    if k == 27:
+        break
+
+    # get current positions of four trackbars
+    l = cv2.getTrackbarPos('L','image')
+    r = cv2.getTrackbarPos('R','image')
+
+    if l < 0:
+        l = 64
+    if r < 0:
+        r = 64
+
+    print (l, r)
+    switchvalue = cv2.getTrackbarPos(switch,'image')
+    if switchvalue == 0:
+        l = r = 64
+
+    message = ("#" + chr(l) + chr(r)).encode(("ascii"))
+    s.send(message)
+    #s.send(chr(l).encode("utf-8"))
+    #s.send(chr(r).encode("utf-8"))
+    #s.send(l.to_bytes(1, byteorder=sys.byteorder))
+    #s.send(r.to_bytes(1, byteorder=sys.byteorder))
+
+
+cv2.destroyAllWindows()
+s.close()
--- Robot/Arduino/simpletcpteleop/simpletcpteleop.ino
+++ Robot/Arduino/simpletcpteleop/simpletcpteleop.ino
@@ -3,36 +3,36 @@ int gamemode = 1;
 
 FRCmotor LeftDriveMotor;
 FRCmotor RightDriveMotor;
-
-int leftspeed = 0;
-int rightspeed = 0;
   
 void setup() {
   Serial.begin(9600);
-  LeftDriveMotor.SetPort(3); //port 3,5,6,9,10,11 on uno
-  LeftDriveMotor.SetPort(5);
+  LeftDriveMotor.SetPort(5); //port 3,5,6,9,10,11 on uno
+  RightDriveMotor.SetPort(9);
   pinMode(13, OUTPUT);
-  digitalWrite(13, INPUT);
+  digitalWrite(13, LOW);
   
 
 }
 
 void loop(){
+  int leftspeed = 0;
+  int rightspeed = 0;
   while (!Serial.available()) {};
   char startlabel = Serial.read();
   if (startlabel == '#') {
     digitalWrite(13, HIGH);
     while (!Serial.available()) {};
-    leftspeed = Serial.read() * 200 / 256 - 100;
-    Serial.println("got left");
-    Serial.println(leftspeed);
-    while (!Serial.available()) {};
-    rightspeed = Serial.read() * 200 / 256 - 100;
-    Serial.println("got right");
-    Serial.println(rightspeed);
-    Serial.println();
+    char l = Serial.read();
+    leftspeed = l * 200 / 127 - 100;
+    //Serial.print("  got left " );
+    Serial.print(leftspeed);
+     while (!Serial.available()) {};
+    char r = Serial.read();
+    rightspeed = r * 200 / 127 - 100;
+    //Serial.print("  got right ");
+    //Serial.println(rightspeed);
+    while(Serial.available()){Serial.read();}
   }
-  
   LeftDriveMotor.Set(leftspeed);
   RightDriveMotor.Set(rightspeed);
 }
